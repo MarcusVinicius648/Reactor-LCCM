7ad7333d53e576e0ac237b25f6c46266f49f0e21
Marcus Vinicius Vieira dos Santos <vinicius09_sanches@hotmail.com>
2025-02-24 17:37:19 -0300

    erro de reconexão

--- server/connection_mqtt.js
+++ server/connection_mqtt.js
@@ -1,12 +1,13 @@
-import {Client} from 'paho-mqtt';
+import {Message,Client} from 'paho-mqtt';
 
 const brokerUrl ="192.168.148.12";
 const port = 9001;
 const clientId = "client-" + Math.random();
+let setpoint = 0;
+
+const client = new Client(brokerUrl,port,clientId);
 
 export function connect(){
-
-    const client = new Client(brokerUrl,port,clientId);
 
     if (client.isConnected()) {
         console.log("Já conectado, desconectando...");
@@ -17,10 +18,11 @@ export function connect(){
     client.connect({
         onSuccess:onSucess,
         onFailure:onFailure,
-
     });
+
     
     if(client.isConnected()){
+        subscribe();
         return true;
     }else{
         return false;
@@ -28,10 +30,10 @@ export function connect(){
 
     
 }
-export {brokerUrl}
+export {brokerUrl};
+export {client};
 function onSucess(){
     console.log("Conectado com Sucesso");
-    
 }
 
 function onFailure(){
@@ -39,4 +41,30 @@ function onFailure(){
    
 }
 
+function subscribe(){
+    client.subscribe("topic_mqtt_temperaturaalvo_reator");
+    client.subscribe("topic_mqtt_powerOnOff_reator");
+}
+
+function onMessage(client,userdata,message){
+
+    
+    if(message.topic == "topic_mqtt_powerOnOff_reator" ){
+        if(message.payload = '1'){
+            console.log('sistema iniciado mediante comando');
+        }else{
+            console.log('sistema encerrado mediante comando');
+        }
+    }if(message.topic == "topic_mqtt_temperaturaalvo_reator"){
+        setpoint = message.payload;
+    };
+
+}
+
+export function pubInfos(topic,message){
+    const payload = new Message(message);
+    payload.destinationName = topic;
+    console.log(payload.payloadString);
+    client.send(payload);
+}
 
